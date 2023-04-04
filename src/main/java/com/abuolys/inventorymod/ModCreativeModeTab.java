0f9106335dc966c0a79a61fedcfef21bab5e5455
abuolyss <makaronas2005@gmail.com>
2023-04-04 16:23:42 +0300

    group photo and creative tab

---
+++ src/main/java/com/abuolys/inventorymod/ModCreativeModeTab.java
@@ -0,0 +1,14 @@
+package com.abuolys.inventorymod;
+
+import com.abuolys.inventorymod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+
+public class ModCreativeModeTab {
+    public static final CreativeModeTab TUTORIAL_TAB = new CreativeModeTab("tutorialtab") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(ModItems.COIN.get());
+        }
+    };
+}
--- src/main/java/com/abuolys/inventorymod/item/ModItems.java
+++ src/main/java/com/abuolys/inventorymod/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.abuolys.inventorymod.item;
 
 import com.abuolys.inventorymod.InventoryMod;
+import com.abuolys.inventorymod.ModCreativeModeTab;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -14,7 +15,12 @@ public class ModItems {
 
 
     public static final RegistryObject<Item> COIN = ITEMS.register("coin",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
+    public static final RegistryObject<Item> GROUP = ITEMS.register("group",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
+
 
     public static void register(IEventBus eventBus) {
 
